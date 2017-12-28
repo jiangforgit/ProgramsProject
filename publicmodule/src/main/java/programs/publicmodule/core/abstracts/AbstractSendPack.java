127867ec7c4c8df946438fee05fb98f78643b8c8
caijiang.chen <caijiang.chen@tpv-tech.com>
2017-12-28 09:56:28 +0800

    redesign send pack

--- publicmodule/src/main/java/programs/publicmodule/core/abstracts/AbstractSendPack.java
+++ publicmodule/src/main/java/programs/publicmodule/core/abstracts/AbstractSendPack.java
@@ -4,7 +4,7 @@ package programs.publicmodule.core.abstracts;
  * Created by caijiang.chen on 2017/11/3.
  */
 
-public class AbstractSendPack {
+public abstract class AbstractSendPack {
 
     protected String getHostIp(){
         return "220.162.239.101";
--- publicmodule/src/main/java/programs/publicmodule/core/impls/SendHeartBeat.java
+++ publicmodule/src/main/java/programs/publicmodule/core/impls/SendHeartBeat.java
@@ -5,7 +5,7 @@ import android.util.Log;
 import java.io.IOException;
 import java.net.DatagramSocket;
 
-import programs.publicmodule.core.abstracts.AbstractSendHeartBeat;
+import programs.publicmodule.core.abstracts.AbstractSendPack;
 import programs.publicmodule.core.entity.HeartBeatPackEntity;
 import programs.publicmodule.core.factorys.UdpSendFactory;
 import programs.publicmodule.core.interfaces.ISendHeartBeat;
@@ -14,7 +14,7 @@ import programs.publicmodule.core.interfaces.ISendHeartBeat;
  * Created by caijiang.chen on 2017/11/3.
  */
 
-public class SendHeartBeat extends AbstractSendHeartBeat implements ISendHeartBeat {
+public class SendHeartBeat extends AbstractSendPack implements ISendHeartBeat {
 
     @Override
     public void sendHeartBeat(DatagramSocket datagramSocket,HeartBeatPackEntity entity) {
@@ -24,6 +24,29 @@ public class SendHeartBeat extends AbstractSendHeartBeat implements ISendHeartBe
             Log.e("SendHeartBeat","sendHeartBeat error");
             e.printStackTrace();
         }
+    }
+
+    private String getHeartBeatXml(HeartBeatPackEntity entity){
+        StringBuilder sb = new StringBuilder();
+        sb.append("<T>" + "<T_H>" + "<requester>" + entity.getRequester()+ "</requester>" + "<version>1.4</version>"
+                + "<category>" + entity.getCategory() + "</category>"+ "<mac />" + "<packetid>" + entity.getPacketid()+ "</packetid>" + "</T_H>");
+
+        sb.append("<T_B>" + "<item cmd=" + "'"+ entity.getCmd() + "'" + " user_id=" + "'"+ entity.getUserid() + "'"
+                +" mobile=" + "'"+ entity.getMobile() + "'");
+
+//        if(isPermitLocation){//允许定位
+//            sb.append(" lat=" + "'" + entity.getLat()+"'" +" lng=" + "'" + entity.getLng() + "'" + " loc="+ "'" + entity.getLoc() + "'" + " radius="+"'"+entity.getRadius()+ "'" + " flag="+"'"+entity.getFlag()+"'" );
+//        }else{
+        sb.append(" lat=" + "'" + ""+"'" +" lng=" + "'" + "" + "'" + " loc="+ "'" + "" + "'" + " radius="+"'"+""+ "'" + " flag="+"'"+"keep_live"+"'" );
+//        }
+
+        sb.append(" udid=" + "'"+ entity.getImei() + "'" +  " cversion=" + "'" + entity.getVersion() + "'" + " time=" + "'" + entity.getTime()+ "'" + "/>"
+                + "</T_B>" + "</T>");
+        return sb.toString();
+    }
+
+    private String getHeartBeatJason(HeartBeatPackEntity entity){
+        return "";
     }
 
 }
